4ef64ba4405ba9776c203bd3791e4b97f010c285
guille <kondra.guille@gmail.com>
2025-04-13 15:06:49 -0300

    agregamos ejercicios python y js

---
+++ JavaScrip/ProyectosJs/Leccion01/MundoPC.js
@@ -0,0 +1,42 @@
+class DispositivoEntrada{
+    //constructor con dos atributos
+    constructor(tipoEntrada,marca){
+        this._tipoEntrada = tipoEntrada;
+        this._marca = marca;
+    }
+    get tipoEntrada(){
+        return this._tipoEntrada;
+    }
+    set tipoEntrada(tipoEntrada){
+        this._tipoEntrada = tipoEntrada;
+    }
+
+    get marca(){
+        return this._marca;
+    }
+    set marca(marca){
+        this._marca = marca
+    }
+}
+
+class Raton extends DispositivoEntrada{
+    static contadorRatones = 0;
+
+    constructor(tipoEntrada,marca){
+        super(tipoEntrada, marca);
+        this._idRaton = ++Raton.contadorRatones;
+    }
+    get idRaton(){
+        return this._idRaton;
+    }
+    toString(){
+        return `Raton: [idRaton: ${this._idRaton}, tipoEntrada: ${this.tipoEntrada}, marca: ${this.marca}]`;
+    }
+}
+
+let raton1 = new Raton('USB','HP')
+console.log(raton1.toString());
+
+let raton2 = new Raton('Bluetooth','Dell')
+console.log(raton2.toString());
+
