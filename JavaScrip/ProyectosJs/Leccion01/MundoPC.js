5c113ad4e1f5d19468ca9074da4ec36631f27a37
guille <kondra.guille@gmail.com>
2025-04-13 21:46:48 -0300

    actualizacion 13 de abril

--- JavaScrip/ProyectosJs/Leccion01/MundoPC.js
+++ JavaScrip/ProyectosJs/Leccion01/MundoPC.js
@@ -40,3 +40,22 @@ console.log(raton1.toString());
 let raton2 = new Raton('Bluetooth','Dell')
 console.log(raton2.toString());
 
+class Teclado extends DispositivoEntrada{
+    static contadorTeclados = 0;
+
+    constructor(tipoEntrada, marca){
+        super(tipoEntrada, marca);
+        this._idTeclado = ++Teclado.contadorTeclados;
+    }
+    get idTeclado(){
+        return this._idTeclado
+    }
+    toString(){
+        return`Teclado: [idTeclado: ${this._idTeclado}, tipoEntrada: ${this._tipoEntrada}, marca: ${this._marca}]`
+    }
+}
+
+let teclado1 = new Teclado("USB", "HP");
+console.log(teclado1.toString());
+let teclado2 = new Teclado("Bluetooth", "MSI");
+console.log(teclado2.toString());
